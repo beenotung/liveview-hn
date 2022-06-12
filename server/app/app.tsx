aa837507002416441f2a94802e9abae582ced0db
Beeno Tung <aabbcc1241@yahoo.com.hk>
2022-06-12 11:21:47 +0800

    open external submit link in new tab

--- server/app/app.tsx
+++ server/app/app.tsx
@@ -64,7 +64,7 @@ function Menu(attrs: {}) {
       {' | '}
       {link('/jobs', 'jobs')}
       {' | '}
-      <a href="https://news.ycombinator.com/submit">submit</a>
+      <a href="https://news.ycombinator.com/submit" target="_blank">submit</a>
     </span>
   )
 }
