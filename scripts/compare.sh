8068b9f7491141709eb13b930fb3c2b410fc26ff
Beeno Tung <aabbcc1241@yahoo.com.hk>
2022-06-27 02:15:04 +0800

    sync with ts-liveview v4

--- scripts/compare.sh
+++ scripts/compare.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+## to sync the application code base with the cloned template
+## you may change the path according to your preference
 set -e
 set -o pipefail
-meld $1 ../ts-liveview/$1
+meld ~/workspace/github.com/beenotung/ts-liveview/$1 $1
