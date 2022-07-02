bbea4f3160212b35191a3e4962abd7ce012518d0
Beeno Tung <aabbcc1241@yahoo.com.hk>
2022-07-02 23:41:23 +0800

    add dev scripts from ts-liveview template

---
+++ scripts/deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+source scripts/config
+npm run build
+if [ "$MODE" == "quick" ]; then
+rsync -SavLP \
+  build \
+  dist \
+  "$user@$host:$root_dir"
+ssh "$user@$host" " \
+source ~/.nvm/nvm.sh \
+&& pm2 reload $pm2_name \
+"
+else
+rsync -SavLP \
+  build \
+  dist \
+  package.json \
+  "$user@$host:$root_dir"
+rsync -SavLP \
+  db/package.json \
+  db/migrations \
+  "$user@$host:$root_dir/db"
+ssh "$user@$host" " \
+source ~/.nvm/nvm.sh \
+&& set -x \\
+&& cd $root_dir \
+&& pnpm i -r \
+&& cd db \
+&& npm run migrate \
+&& pm2 reload $pm2_name \
+"
+fi
